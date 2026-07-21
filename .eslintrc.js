22cdcbad8841b6a2ac05ceaaa060e711f17ec7be
Fixxia <97739814+Zheke32174@users.noreply.github.com>
2026-07-21 08:02:19 -0400

    Fix ESLint core rule and cross-platform formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,11 +17,11 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': ['error', { endOfLine: 'lf' }],
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/explicit-function-return-type': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/prefer-const': 'error',
+    'prefer-const': 'error',
     'no-console': 'warn',
   },
   ignorePatterns: ['dist/', 'build/', 'node_modules/', '*.js', '*.d.ts'],
